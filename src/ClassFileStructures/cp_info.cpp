ceca896a5c8f479edceef50336757b627b6e039b
GabrielBessa <gabrielbessarofl@gmail.com>
2018-09-26 20:15:31 -0300

    constant pool adicionado

---
+++ src/ClassFileStructures/cp_info.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include "../UsingUs.hpp"
+#include <vector>
+#include "cp_info.hpp"
+
+enum enum_cp_tags {
+  CONSTANT_Class = 7,
+  CONSTANT_Fieldref = 9,
+  CONSTANT_Methodref = 10,
+  CONSTANT_InterfaceMethodref = 11,
+  CONSTANT_String = 8,
+  CONSTANT_Integer = 3,
+  CONSTANT_Float = 4,
+  CONSTANT_Long = 5,
+  CONSTANT_Double = 6,
+  CONSTANT_NameAndType = 12,
+  CONSTANT_Utf8 = 1,
+  CONSTANT_MethodHandle = 15,
+  CONSTANT_MethodType = 16,
+  CONSTANT_InvokeDynamic = 18
+};
+
+vector<u1> cp_tags_values {
+  CONSTANT_Class,
+  CONSTANT_Fieldref,
+  CONSTANT_Methodref,
+  CONSTANT_InterfaceMethodref,
+  CONSTANT_String,
+  CONSTANT_Integer,
+  CONSTANT_Float,
+  CONSTANT_Long,
+  CONSTANT_Double,
+  CONSTANT_NameAndType,
+  CONSTANT_Utf8,
+  CONSTANT_MethodHandle,
+  CONSTANT_MethodType,
+  CONSTANT_InvokeDynamic
+;}
+
+class cp_info
+{
+private:
+
+public:
+  u1 tag;
+  vector<u1> info;
+}
+
+int main(){
+
+  return 0;
+}
