a33a05d0b704bd8d98a5c06750ac1656ffdabb5d
maffei2443 <leoitu22hotmail.com@gmail.com>
2018-10-02 23:47:12 -0300

    improbing readbility, just a bit

--- tst/readClassFile.test.cpp
+++ tst/readClassFile.test.cpp
@@ -4,15 +4,16 @@
 #include <cstring>
 #include "../src/UsingUs.hpp"
 void read_us(void* buf, int n , FILE * f) {
+  u1* ptr = (u1*)buf;
   for(int i = n-1; i >= 0; i--) {
-    fread( &((u1*)buf)[i], 1, 1, f );
+    fread( &ptr[i], 1, 1, f );
   }
 }
 
 int main() {
-  u2 buf;
+  u4 buf;
   FILE * f;
   f = fopen("double_aritmetica.class", "rb");
-  read_us(&buf, 2, f);
+  read_us(&buf, 4, f);
   std::cout << std::hex << std::showbase << std::uppercase << buf << std::endl;
 }
