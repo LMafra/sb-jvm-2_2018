cf4dcb8fc2c3d4f813e2b187c0994cf7d3c18d98
maffei2443 <leoitu22hotmail.com@gmail.com>
2018-10-08 20:14:00 -0300

    bug de leitura é pra ter sido resolvido

--- src/ClassFileStructures/cp_info.cpp
+++ src/ClassFileStructures/cp_info.cpp
@@ -2,54 +2,54 @@
 #include "read_us.hpp"
 #include <cstdio>
 void CONSTANT_Class_info :: fill_from(FILE * f) {
-  read_us(&this->tag, sizeof(this->tag), f);
+  // read_us(&this->tag, sizeof(this->tag), f);
   read_us(&this->name_index, sizeof(this->name_index), f);
 }
 void CONSTANT_Fieldref_info :: fill_from(FILE * f) {
-  read_us(&this->tag, sizeof(this->tag), f);
+  // read_us(&this->tag, sizeof(this->tag), f);
   read_us(&this->class_index, sizeof(this->class_index), f);
   read_us(&this->name_and_type_index, sizeof(this->name_and_type_index), f);
 }
 void CONSTANT_Methodref_info :: fill_from(FILE * f) {
-  read_us(&this->tag, sizeof(this->tag), f);
+  // read_us(&this->tag, sizeof(this->tag), f);
   read_us(&this->class_index, sizeof(this->class_index), f);
   read_us(&this->name_and_type_index, sizeof(this->name_and_type_index), f);
 }
 void CONSTANT_InterfaceMethodref_info :: fill_from(FILE * f) {
-  read_us(&this->tag, sizeof(this->tag), f);
+  // read_us(&this->tag, sizeof(this->tag), f);
   read_us(&this->class_index, sizeof(this->class_index), f);
   read_us(&this->name_and_type_index, sizeof(this->name_and_type_index), f);
 }
 void CONSTANT_String_info :: fill_from(FILE * f) {
-  read_us(&this->tag, sizeof(this->tag), f);
+  // read_us(&this->tag, sizeof(this->tag), f);
   read_us(&this->string_index, sizeof(this->string_index), f);
 }
 void CONSTANT_Integer_info :: fill_from(FILE * f) {
-  read_us(&this->tag, sizeof(this->tag), f);
+  // read_us(&this->tag, sizeof(this->tag), f);
   read_us(&this->bytes, sizeof(this->bytes), f);
 }
 void CONSTANT_Float_info :: fill_from(FILE * f) {
-  read_us(&this->tag, sizeof(this->tag), f);
+  // read_us(&this->tag, sizeof(this->tag), f);
   read_us(&this->bytes, sizeof(this->bytes), f);
 }
 void CONSTANT_Long_info :: fill_from(FILE * f) {
-  read_us(&this->tag, sizeof(this->tag), f);
+  // read_us(&this->tag, sizeof(this->tag), f);
   read_us(&this->high_bytes, sizeof(this->high_bytes), f);
   read_us(&this->low_bytes, sizeof(this->low_bytes), f);
 }
 void CONSTANT_Double_info :: fill_from(FILE * f) {
-  read_us(&this->tag, sizeof(this->tag), f);
+  // read_us(&this->tag, sizeof(this->tag), f);
   read_us(&this->high_bytes, sizeof(this->high_bytes), f);
   read_us(&this->low_bytes, sizeof(this->low_bytes), f);
 }
 void CONSTANT_NameAndType_info :: fill_from(FILE * f) {
-  read_us(&this->tag, sizeof(this->tag), f);
+  // read_us(&this->tag, sizeof(this->tag), f);
   read_us(&this->name_index, sizeof(this->name_index), f);
   read_us(&this->descriptor_index, sizeof(this->descriptor_index), f);
 }
 // Ver essa leitura de ponteiro... Alocar espaco antes!
 void CONSTANT_Utf8_info :: fill_from(FILE * f) {
-  read_us(&this->tag, sizeof(this->tag), f);
+  // read_us(&this->tag, sizeof(this->tag), f);
   read_us(&this->length, sizeof(this->length), f);
   this->bytes = (u1*)malloc(sizeof(u1) * this->length);
   read_us(&this->bytes, this->length, f);
