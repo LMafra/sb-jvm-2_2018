570fc4fc185fb5f8ba17009adfdd99c105f4d22b
maffei2443 <leoitu22hotmail.com@gmail.com>
2018-10-10 13:30:40 -0300

    eleting fat

--- src/ClassFileStructures/attribute_info.cpp
+++ src/ClassFileStructures/attribute_info.cpp
@@ -8,64 +8,21 @@ void ConstantValue_attribute  :: fill_from(FILE * f) {
 	read_us(&constantvalue_index, sizeof(constantvalue_index), f);
 }
 
-void StackMapTable_attribute  :: fill_from(FILE * f) {
-	read_us(&attribute_name_index, sizeof(attribute_name_index), f);
-	read_us(&attribute_length, sizeof(attribute_length), f);
-	read_us(&number_of_entries, sizeof(number_of_entries), f);
-	read_us(&entries, sizeof(entries), f);
-	read_us(&verification_type_info, sizeof(verification_type_info), f);
-}
-
-void EnclosingMethod_attribute  :: fill_from(FILE * f) {
-	read_us(&attribute_name_index, sizeof(attribute_name_index), f);
-	read_us(&attribute_length, sizeof(attribute_length), f);
-	read_us(&class_index, sizeof(class_index), f);
-	read_us(&method_index, sizeof(method_index), f);
-}
-
-void Signature_attribute  :: fill_from(FILE * f) {
-	read_us(&attribute_name_index, sizeof(attribute_name_index), f);
-	read_us(&attribute_length, sizeof(attribute_length), f);
-	read_us(&signature_index, sizeof(signature_index), f);
-}
-
-void SourceDebugExtension_attribute  :: fill_from(FILE * f) {
-	read_us(&attribute_name_index, sizeof(attribute_name_index), f);
-	read_us(&attribute_length, sizeof(attribute_length), f);
-	read_us(&debug_extension, sizeof(debug_extension), f);
-}
-
-void LocalVariableTable_attribute  :: fill_from(FILE * f) {
-	read_us(&attribute_name_index, sizeof(attribute_name_index), f);
-	read_us(&attribute_length, sizeof(attribute_length), f);
-	read_us(&local_variable_table_length, sizeof(local_variable_table_length), f);
-	read_us(&local_variable_table, sizeof(local_variable_table), f);
-}
-
 void Deprecated_attribute  :: fill_from(FILE * f) {
 	read_us(&attribute_name_index, sizeof(attribute_name_index), f);
 	read_us(&attribute_length, sizeof(attribute_length), f);
 }
 
-// void element_value  :: fill_from(FILE * f) {
-	read_us(&tag, sizeof(tag), f);
-	read_us(&value, sizeof(value), f);
-	read_us(&}, sizeof(}), f);
-}
-
-void RuntimeVisibleParameterAnnotations_attribute  :: fill_from(FILE * f) {
+void SourceFile_attribute  :: fill_from(FILE * f) {
 	read_us(&attribute_name_index, sizeof(attribute_name_index), f);
 	read_us(&attribute_length, sizeof(attribute_length), f);
-	read_us(&num_parameters, sizeof(num_parameters), f);
-	read_us(&parameter_annotations, sizeof(parameter_annotations), f);
-	read_us(&annotations, sizeof(annotations), f);
+	read_us(&sourcefile_index, sizeof(sourcefile_index), f);
 }
 
-void AnnotationDefault_attribute  :: fill_from(FILE * f) {
+void LineNumberTable_attribute  :: fill_from(FILE * f) {
 	read_us(&attribute_name_index, sizeof(attribute_name_index), f);
 	read_us(&attribute_length, sizeof(attribute_length), f);
-	read_us(&default_value, sizeof(default_value), f);
-	read_us(&std, sizeof(std), f);
-	read_us(&attribute_info, sizeof(attribute_info), f);
+	read_us(&line_number_table_length, sizeof(line_number_table_length), f);
+	read_us(&line_number_table, sizeof(line_number_table), f);
 }
 
--- src/ClassFileStructures/attribute_info.hpp
+++ src/ClassFileStructures/attribute_info.hpp
@@ -25,23 +25,18 @@ class Code_attribute {public:
   ExceptionTable * exception_table;
   u2 attributes_count;
   attribute_info * attributes;
-  ~Code_attribute() {
-    free(this->code);
-    free(this->exception_table);
-  }
+};
+
+// 4.7.15
+class Deprecated_attribute {public:
+  u2 attribute_name_index;
+  u4 attribute_length;
 };
 
 // 4.7.4
 // require "attribute_info_aux.hpp" cause of stack_map_frame
 #include "attribute_info_aux.hpp"
-class StackMapTable_attribute {public:
-  u2 attribute_name_index;
-  u4 attribute_length;
-  u2 number_of_entries;
-  stack_map_frame * entries;
-};
 
-union  verification_type_info;
 
 // Exceptions Attributes
 class Exceptions_attribute {public:
@@ -59,39 +54,12 @@ class InnerClasses_attribute {
   Classes * classes;
 };
 
-// 4.7.7
-class EnclosingMethod_attribute {public:
-  u2 attribute_name_index;
-  u4 attribute_length;
-  u2 class_index;
-  u2 method_index;
-};
-
-// 4.7.8
-class Synthetic_attribute {public:
-  u2 attribute_name_index;
-  u4 attribute_length;
-};
-
-// 4.7.9
-class Signature_attribute {public:
-  u2 attribute_name_index;
-  u4 attribute_length;
-  u2 signature_index;
-};
 
 // 4.7.10
 class SourceFile_attribute {public:
   u2 attribute_name_index;
   u4 attribute_length;
   u2 sourcefile_index;
-};
-
-// 4.7.11
-class SourceDebugExtension_attribute {public:
-  u2 attribute_name_index;
-  u4 attribute_length;
-  u1 * debug_extension;
 };
 
 // 4.7.12
@@ -109,68 +77,5 @@ class LocalVariableTable_attribute {public:
   Local_variable_table * local_variable_table; // local_variable_table[local_variable_table_length]
 };
 
-// 4.7.14
-class LocalVariableTypeTable_attribute {public:
-  u2 attribute_name_index;
-  u4 attribute_length;
-  u2 local_variable_type_table_length;
-  Local_variable_type_table * local_variable_type_table;
-};
-
-// 4.7.15
-class Deprecated_attribute {public:
-  u2 attribute_name_index;
-  u4 attribute_length;
-};
-
-// 4.7.16
-class RuntimeVisibleAnnotations_attribute {public:
-  u2 attribute_name_index;
-  u4 attribute_length;
-  u2 num_annotations;
-  annotation * annotations;
-};
-
-// 4.7.16.1
-class element_value;
-// class element_value {public:
-//   u1 tag;
-//   UnionValue value;
-// };
-
-// 4.7.17
-class RuntimeInvisibleAnnotations_attribute {public:
-  u2 attribute_name_index;
-  u4 attribute_length;
-  u2 num_annotations;
-  annotation * annotations; // annotations[num_annotations]
-};
-
-// 4.7.18
-class Parameter_annotations;  // depends on annotation
-class RuntimeVisibleParameterAnnotations_attribute {public:
-  u2 attribute_name_index;
-  u4 attribute_length;
-  u1 num_parameters;
-  Parameter_annotations * parameter_annotations; // parameter_annotations[num_parameters]
-  annotation * annotations; // annotations[num_annotations]
-};
-
-// 4.7.19
-class RuntimeInvisibleParameterAnnotations_attribute {public:
-  u2 attribute_name_index;
-  u4 attribute_length;
-  u1 num_parameters;
-  Parameter_annotations * parameter_annotations; // parameter_annotations[num_parameters]
-};
-
-// 4.7.20
-class AnnotationDefault_attribute {public:
-  u2 attribute_name_index;
-  u4 attribute_length;
-  element_value default_value;
-};
-using namespace std;
 /// \brief Esta classe é usada pela classes ClassFile,
 /// field_info, method_info e Code_attributes.
-class attribute_info;
