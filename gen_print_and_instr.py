879d74381f38c5b293fd02f6b5f397e5866cc646
CrimsonCrown <arthurscouto@outlook.com>
2018-11-01 10:36:46 -0300

    faz script de python gerar output de nome de instrucao

--- gen_print_and_instr.py
+++ gen_print_and_instr.py
@@ -49,9 +49,40 @@ def lazy_gen(filename, prefix):
 	print('QTD :: ',len(lis))
 	return [lis, idx]
 
+def lazy_genp(filename, prefix):
+	lines = [i.strip() for i in open(filename)]
+	# print(lines)
+	it = 0
+	c = -1
+	sign = 'void ' + prefix
+	lis = []
+	idx = []
+	while it < len(lines)  :
+		ln = lines[it]
+		while re.findall('enum', ln):
+			it += 1
+			ln = lines[it]
+		if re.findall('=', ln):
+			# print('with equeal :: ', ln)
+			# print(re.findall(r'([^\s]+).+?(\d+)', ln))
+			c = int(re.findall(r'([^\s]+).+?(\d+)', ln)[0][1])
+		else:
+			c += 1
+		
+		idx.append(c)  # to do the array of pointers
+		
+		# print('c == ', str(c))
+		lis.append(re.findall(r'([^\s]+)', ln)[0])
+		it += 1
+	with open(prefix+filename+'.hpp', 'w') as ff:
+		for i in lis:
+			ff.write(sign + i + ' {\n\tstd::cout << "' + i[4:100] + '";\n}\n')
+	print('QTD :: ',len(lis))
+	return [lis, idx]
+
 def main():
 	array_function('exec_', lazy_gen('instr', 'exec_'), vetname='exec_vet')
-	array_function('print_', lazy_gen('instr', 'print_'), vetname='print_vet')
+	array_function('print_', lazy_genp('instr', 'print_'), vetname='print_vet')
 	return 0
 if __name__ == '__main__':
 	main()
