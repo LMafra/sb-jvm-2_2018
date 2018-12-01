c1fa44ace0fe70c5474f66284de3ec744266cb1f
maffei2443 <leoitu22hotmail.com@gmail.com>
2018-12-01 18:43:41 -0200

    gambiarra no getstatic

--- src/Instructions/instr3.cpp
+++ src/Instructions/instr3.cpp
@@ -1,145 +1,48 @@
-void exec_jvm_istore(){
+#include "instructions.hpp"
+#include "exec_instr.hpp"
+#include "../UsingUs.hpp"
+#include "../VMGlobals.hpp"
+#include <string.h>
 
-}
+void exec_jvm_istore(){}
+void exec_jvm_istore_0(){}
+void exec_jvm_istore_1(){}
+void exec_jvm_istore_2(){}
+void exec_jvm_istore_3(){}
 
 
-void exec_jvm_lstore(){
+void exec_jvm_lstore(){}
+void exec_jvm_lstore_0(){}
+void exec_jvm_lstore_1(){}
+void exec_jvm_lstore_2(){}
+void exec_jvm_lstore_3(){}
 
-}
 
+void exec_jvm_fstore(){}
+void exec_jvm_fstore_0(){}
+void exec_jvm_fstore_1(){}
+void exec_jvm_fstore_2(){}
+void exec_jvm_fstore_3(){}
 
-void exec_jvm_fstore(){
 
-}
+void exec_jvm_dstore(){}
+void exec_jvm_dstore_0(){}
+void exec_jvm_dstore_1(){}
+void exec_jvm_dstore_2(){}
+void exec_jvm_dstore_3(){}
 
 
-void exec_jvm_dstore(){
 
-}
+void exec_jvm_astore(){}
+void exec_jvm_astore_0(){}
+void exec_jvm_astore_1(){}
+void exec_jvm_astore_2(){}
+void exec_jvm_astore_3(){}
 
 
-void exec_jvm_astore(){
-
-}
-
-
-void exec_jvm_istore_0(){
-
-}
-
-
-void exec_jvm_istore_1(){
-
-}
-
-
-void exec_jvm_istore_2(){
-
-}
-
-
-void exec_jvm_istore_3(){
-
-}
-
-
-void exec_jvm_lstore_0(){
-
-}
-
-
-void exec_jvm_lstore_1(){
-
-}
-
-
-void exec_jvm_lstore_2(){
-
-}
-
-
-void exec_jvm_lstore_3(){
-
-}
-
-
-void exec_jvm_fstore_0(){
-
-}
-
-
-void exec_jvm_fstore_1(){
-
-}
-
-
-void exec_jvm_fstore_2(){
-
-}
-
-
-void exec_jvm_fstore_3(){
-
-}
-
-
-void exec_jvm_dstore_0(){
-
-}
-
-
-void exec_jvm_dstore_1(){
-
-}
-
-
-void exec_jvm_dstore_2(){
-
-}
-
-
-void exec_jvm_dstore_3(){
-
-}
-
-
-void exec_jvm_astore_0(){
-
-}
-
-
-void exec_jvm_astore_1(){
-
-}
-
-
-void exec_jvm_astore_2(){
-
-}
-
-
-void exec_jvm_astore_3(){
-
-}
-
-
-void exec_jvm_iastore(){
-
-}
-
-
-void exec_jvm_lastore(){
-
-}
-
-
-void exec_jvm_fastore(){
-
-}
-
-
-void exec_jvm_dastore(){
-
-}
+void exec_jvm_iastore(){}
+void exec_jvm_lastore(){}
+void exec_jvm_fastore(){}
+void exec_jvm_dastore(){}
 
 
--- src/Instructions/instr8.cpp
+++ src/Instructions/instr8.cpp
@@ -29,8 +29,6 @@ void exec_jvm_tableswitch(){
     PC = instr_pc + default_val;
   else
     PC = instr_pc + PC[index - low_val];
-
-
 }
 void exec_jvm_lookupswitch(){
 
@@ -53,8 +51,26 @@ void exec_jvm_areturn(){
 void exec_jvm_return(){
 
 }
-void exec_jvm_getstatic(){
 
+void exec_jvm_getstatic(){
+  u2 index = offset16_from_instr();
+  Frame_type * frame = &frame_stack.top();
+  cp_info * cp = frame->inst->my_class_ptr->constant_pool;
+  CONSTANT_Fieldref_info * symbolic_reference_to_field =
+   (CONSTANT_Fieldref_info*)&cp[index];
+  // The constant_pool entry at that index must be a
+  // CONSTANT_Class_info (§4.4.1) structure representing a class or
+  //  interface type
+  u2 class_index = symbolic_reference_to_field->class_index;
+  CONSTANT_Class_info * class_info = (CONSTANT_Class_info *)&cp[class_index];
+  u2 name_index = class_info->name_index;
+  // The constant_pool entry at that index must be a
+  // CONSTANT_Utf8_info (§4.4.7) structure representing 
+  // a valid binary class or interface name encoded in internal form (§4.2.1).
+  CONSTANT_Utf8_info * utf8_info = (CONSTANT_Utf8_info *)&cp[name_index];
+
+  // gambiarra pq essa porcaria tá mal descrita e bem ruim de entender
+  jvm_push_reference((void*)NULL);
 }
 void exec_jvm_putstatic(){
 
