410ac372741068255280e5e03f24fe0455df6923
maffei2443 <leoitu22hotmail.com@gmail.com>
2018-12-01 02:19:07 -0200

    README para explicar a estrutura das instrucoes

--- src/Instructions/instr1.cpp
+++ src/Instructions/instr1.cpp
@@ -34,40 +34,48 @@ void exec_jvm_ldc2_w(){
 
 }
 
-void exec_jvm_iload(){
-
-}
-
 
 // maffei
 /// Empilha em jvm_stack a variável na N-ehsima posicao do vetor de variaveis locais.
 /// É chamada pelas instruções iload_<n> da jvm.
-void exec_jvm_iload(u1 N) {jvm_push( frame_stack.top().variaveis_locais[N] );}
-void exec_jvm_iload_0() {exec_jvm_iload(0);}
-void exec_jvm_iload_1() {exec_jvm_iload(1);}
-void exec_jvm_iload_2() {exec_jvm_iload(2);}
-void exec_jvm_iload_3() {exec_jvm_iload(3);}
+inline static void exec_jvm_iload_X(u1 N) {
+  jvm_push( frame_stack.top().variaveis_locais[N] );
+}
+void exec_jvm_iload() {
+
+}
+void exec_jvm_iload_0() {exec_jvm_iload_X((u1)0);incpc(1);}
+void exec_jvm_iload_1() {exec_jvm_iload_X((u1)1);incpc(1);}
+void exec_jvm_iload_2() {exec_jvm_iload_X((u1)2);incpc(1);}
+void exec_jvm_iload_3() {exec_jvm_iload_X((u1)3);incpc(1);}
 
 // maffei
 /// empilha um long do vetor de variaveis locais
-void exec_jvm_lload(u1 N) {exec_jvm_iload(N); exec_jvm_iload(N+1);}
-void exec_jvm_lload_0() {exec_jvm_lload(0);}
-void exec_jvm_lload_1() {exec_jvm_lload(1);}
-void exec_jvm_lload_2() {exec_jvm_lload(2);}
-void exec_jvm_lload_3() {exec_jvm_lload(3);}
+inline static void exec_jvm_lload_X(u1 N) {exec_jvm_iload_X(N); exec_jvm_iload_X(N+1);}
+
+void exec_jvm_lload() {
+}
+void exec_jvm_lload_0(){exec_jvm_lload_X((u1)0);incpc(1);}
+void exec_jvm_lload_1(){exec_jvm_lload_X((u1)1);incpc(1);}
+void exec_jvm_lload_2(){exec_jvm_lload_X((u1)2);incpc(1);}
+void exec_jvm_lload_3(){exec_jvm_lload_X((u1)3);incpc(1);}
 
 // maffei
 /// empilha um float do vetor de variaveis locais
-void exec_jvm_fload(u1 N) {exec_jvm_iload(N);}
-void exec_jvm_fload_0() {exec_jvm_fload(0);}
-void exec_jvm_fload_1() {exec_jvm_fload(1);}
-void exec_jvm_fload_2() {exec_jvm_fload(2);}
-void exec_jvm_fload_3() {exec_jvm_fload(3);}
+inline static void exec_jvm_fload_X(u1 N) {exec_jvm_iload_X(N);}
+void exec_jvm_fload() {
+
+}
+void exec_jvm_fload_0() {exec_jvm_fload_X(0);incpc(1);}
+void exec_jvm_fload_1() {exec_jvm_fload_X(1);incpc(1);}
+void exec_jvm_fload_2() {exec_jvm_fload_X(2);incpc(1);}
+void exec_jvm_fload_3() {exec_jvm_fload_X(3);incpc(1);}
 
 // maffei
 /// empilha um double do vetor de variaveis locais
-void exec_jvm_dload(u1 N){exec_jvm_iload(N);exec_jvm_iload(N+1);}
-void exec_jvm_dload_0(){exec_jvm_dload(0);}
-void exec_jvm_dload_1(){exec_jvm_dload(1);}
-void exec_jvm_dload_2(){exec_jvm_dload(2);}
-void exec_jvm_dload_3(){exec_jvm_dload(3);}
+void exec_jvm_dload_X(u1 N){exec_jvm_iload_X(N);exec_jvm_iload_X(N+1);}
+void exec_jvm_dload(){}
+void exec_jvm_dload_0(){exec_jvm_dload_X(0);}
+void exec_jvm_dload_1(){exec_jvm_dload_X(1);}
+void exec_jvm_dload_2(){exec_jvm_dload_X(2);}
+void exec_jvm_dload_3(){exec_jvm_dload_X(3);}
